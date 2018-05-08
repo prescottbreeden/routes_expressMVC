d776103a3e765b5a1d06cf1d18991fee9df37e89
Prescott Breeden <prescott.breeden@gmail.com>
2018-05-07 19:56:02 -0700

    file created

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+
+app.use('view engine', 'ejs');
+app.use('')
+
+app.listen(8000, function() {
+    console.log("Power Overwhelming...")
+})
+
+model.exports = app;
